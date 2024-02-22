0e055bce57774665590fde73f33f73a13d00aa0d
ecyr20 <ecyr20@tamu.edu>
2024-02-22 13:41:01 -0600

    Wiping of old Chemistry Cafe
    The old files have been wiped and replaced with a default react web app.
    
    Use npm install after cloning to install node_modules.

--- login/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
- $past = time() - 100; 
- //this makes the time in the past to destroy the cookie 
- setcookie("chemdb_id", "gone", $past, "/"); 
- //header("Location: http://chemdb:3141/login/login.php");
- header("Location: /login/login.php");
- ?> 
-
