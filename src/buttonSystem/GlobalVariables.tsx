ac0781e12f0f4815b4014ed80507d2f0626064b1
Brandon Longuet <37945524+AmazingBrandonL@users.noreply.github.com>
2024-03-25 14:46:21 -0500

    Updating Global Variables
    ADDED: Reaction, Species, and TagMechanism global variables that all webpages can use

--- src/buttonSystem/GlobalVariables.tsx
+++ src/buttonSystem/GlobalVariables.tsx
@@ -38,3 +38,60 @@ export const useMechanismUuid = () => {
 
     return { mechanismUuid, handleMechanismClick };
 };
+
+export const useReactionUuid = () => {
+    const [reactionUuid, setReactionUuid] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedReactionUuid = localStorage.getItem('reaction_uuid');
+        if (storedReactionUuid) {
+            setReactionUuid(storedReactionUuid);
+        }
+    }, []);
+
+    const handleReactionClick = (uuid: string) => {
+        localStorage.setItem('reaction_uuid', uuid);
+        console.log(uuid);
+        setReactionUuid(uuid);
+    };
+
+    return { reactionUuid, handleReactionClick };
+};
+
+export const useSpeciesUuid = () => {
+    const [speciesUuid, setSpeciesUuid] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedSpeciesUuid = localStorage.getItem('species_uuid');
+        if (storedSpeciesUuid) {
+            setSpeciesUuid(storedSpeciesUuid);
+        }
+    }, []);
+
+    const handleSpeciesClick = (uuid: string) => {
+        localStorage.setItem('species_uuid', uuid);
+        console.log(uuid);
+        setSpeciesUuid(uuid);
+    };
+
+    return { speciesUuid, handleSpeciesClick };
+};
+
+export const useTagMechanismUuid = () => {
+    const [tagMechanismUuid, setTagMechanismUuid] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedTagMechanismUuid = localStorage.getItem('tag_mechanism_uuid');
+        if (storedTagMechanismUuid) {
+            setTagMechanismUuid(storedTagMechanismUuid);
+        }
+    }, []);
+
+    const handleTagMechanismClick = (uuid: string) => {
+        localStorage.setItem('tag_mechanism_uuid', uuid);
+        console.log(uuid);
+        setTagMechanismUuid(uuid);
+    };
+
+    return { tagMechanismUuid, handleTagMechanismClick };
+};
