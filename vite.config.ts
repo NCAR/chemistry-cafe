6c27d662c43dcba2116203b38cc2fef3637a1078
SydZero <sydferris@gmail.com>
2024-09-19 18:55:56 -0500

    update docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Accept connections from outside
+    port: 4173, // You can set this to your desired port
+  },
+});
+
