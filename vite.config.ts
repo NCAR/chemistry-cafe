dc3a1d6826474da822760f2fd28791bee37e2c4c
SydZero <sydferris@gmail.com>
2024-09-22 16:55:31 -0500

    update vite config for compose file

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Accept connections from outside
-    port: 4173, // You can set this to your desired port
+    host: '0.0.0.0',
+    port: 4173,
   },
 });
 
