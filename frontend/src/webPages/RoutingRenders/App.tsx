6e0b7dc5ebb0b9356405701aaa108075e5d44058
SydZero <63630743+SydZero@users.noreply.github.com>
2024-10-17 17:03:06 -0700

    banner added

--- frontend/src/webPages/RoutingRenders/App.tsx
+++ frontend/src/webPages/RoutingRenders/App.tsx
@@ -4,11 +4,15 @@ import LoggedIn from '../LogIn/loggedIn';
 import FamilyPage from '../Family/family';
 import LogIn from '../LogIn/logIn';
 import RoleManagement from '../Roles/RoleManagement';
+import React from 'react';
+import './App.css'; // Assuming CSS is applied globally
+import Banner from '../Components/CookieBanner';
 
 function App() {
+
     return (
         <div>
-            
+            <Banner />
             <Routes>
                 <Route path="/" element={<LogIn />} />
                 <Route path="/LoggedIn" element={<LoggedIn />} />
