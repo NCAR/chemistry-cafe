de00591596397e2c68cad189e890b3de814ab395
SydZero <sydferris@gmail.com>
2024-10-23 18:31:02 -0500

    add font

--- frontend/src/webPages/RoutingRenders/App.tsx
+++ frontend/src/webPages/RoutingRenders/App.tsx
@@ -6,19 +6,31 @@ import LogIn from '../LogIn/logIn';
 import RoleManagement from '../Roles/RoleManagement';
 import './App.css'; // Assuming CSS is applied globally
 import Banner from '../Components/CookieBanner';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Create a custom theme with your font
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Poppins', sans-serif",
+    
+  },
+});
+
 
 function App() {
 
     return (
         <div>
-            <Banner />
-            <Routes>
-                <Route path="/" element={<LogIn />} />
-                <Route path="/LoggedIn" element={<LoggedIn />} />
-                <Route path="/FamilyPage" element={<FamilyPage />} />
-                <Route path="/Roles" element={<RoleManagement />} />
-                <Route path="/Settings" element={<Settings />} />
-            </Routes>
+            <ThemeProvider theme={theme}>       
+                <Banner />
+                <Routes>
+                    <Route path="/" element={<LogIn />} />
+                    <Route path="/LoggedIn" element={<LoggedIn />} />
+                    <Route path="/FamilyPage" element={<FamilyPage />} />
+                    <Route path="/Roles" element={<RoleManagement />} />
+                    <Route path="/Settings" element={<Settings />} />
+                </Routes>
+            </ThemeProvider>
         </div>
         
     );
