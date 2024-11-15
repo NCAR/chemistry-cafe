d7bee213f1b155ea58c0f86ce91163316cee6540
SydZero <sydferris@gmail.com>
2024-11-15 15:55:41 -0600

    Changed connection to db for tests

--- backend/Tests/DBConnection.cs
+++ backend/Tests/DBConnection.cs
@@ -1,9 +1,17 @@
-﻿// using MySqlConnector;
+﻿using MySqlConnector;
 
-// namespace Chemistry_Cafe_API.Tests
-// {
-//     public class DBConnection
-//     {
-//         public static MySqlDataSource DataSource = new MySqlDataSource("Server=chemisty-cafe.cl8uuceq2rud.us-east-1.rds.amazonaws.com;User ID=cafeadmin;Password=cafeadmin;Port=3306;Database=Testing");
-//     }
-// }
+namespace Chemistry_Cafe_API.Tests
+{
+    public class DBConnection
+    {
+        private static string server = "mysql";
+        private static string user = System.Environment.GetEnvironmentVariable("MYSQL_USER") ?? "defaultUser";
+        private static string password = System.Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "defaultPassword";
+        private static string database = System.Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "defaultDatabase";
+        private static string port = "3306"; // Default MySQL port
+
+        public static MySqlDataSource DataSource = new MySqlDataSource(
+            $"Server={server};User ID={user};Password={password};Port={port};Database={database}"
+        );
+    }
+}
