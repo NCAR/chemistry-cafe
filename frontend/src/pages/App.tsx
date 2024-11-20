99b1ff5381938580e9ef60be40457fdbe38666e8
Kyle Shores <kyle.shores44@gmail.com>
2024-11-20 10:52:41 -0600

    removing nonce

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -10,41 +10,33 @@ import { AuthProvider } from './AuthContext';
 import ProtectedRoute from '../components/ProtectedRoute';
 import Banner from '../components/CookieBanner';
 
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-
-const cache = createCache({
-  key: 'mui',
-  nonce: '1A59E59F-7913-4CA8-AF58-70A5A95C7F60',
-});
-
 function App() {
     return (
-        <CacheProvider value={cache}>
+        <>
             {/* <AccessibilityWidget /> */}
             <AuthProvider>
-            <Banner />
+                <Banner />
                 <Routes>
                     <Route path="/" element={<LogIn />} />
                     <Route path="/LoggedIn" element={<LoggedIn />} />
                     <Route path="/FamilyPage" element={<FamilyPage />} />
                     <Route path="/Settings" element={<Settings />} />
                     <Route path="/unauthorized" element={<NoAccess />} />
-                    
+
                     {/* Protected route for the Roles page */}
-                    <Route 
-                        path="/Roles" 
+                    <Route
+                        path="/Roles"
                         element={
                             <ProtectedRoute requiredRole="admin">
                                 <RoleManagement />
                             </ProtectedRoute>
-                        } 
+                        }
                     />
 
-                 
+
                 </Routes>
             </AuthProvider>
-        </CacheProvider>
+        </>
     );
 }
 
