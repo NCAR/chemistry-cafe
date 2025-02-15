b5020ae0f4151c6d69df1a1f902048bf079e3e7f
LucientZ <koblenzpoisiden@gmail.com>
2025-02-15 15:52:01 -0600

    Removed local GoogleOAuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./pages/App";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -8,17 +7,11 @@ import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
-const url = import.meta.env.VITE_REACT_APP_OAUTH_CLIENT_ID;
 
 root.render(
   <BrowserRouter>
-    <GoogleOAuthProvider
-      clientId={url}
-      nonce={"F04DAE83-E065-4AB5-904F-C2E5E3B3390C"}
-    >
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </GoogleOAuthProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>,
 );
