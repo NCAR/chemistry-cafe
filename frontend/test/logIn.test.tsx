e3fa57579e25437ad8e5cfd5a6a6816fb32f8faa
LucientZ <koblenzpoisiden@gmail.com>
2025-02-23 15:54:54 -0600

    Added tests with other user info cases

--- frontend/test/logIn.test.tsx
+++ frontend/test/logIn.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, beforeEach, afterEach, vi } from "vitest";
+import { describe, expect, it, beforeEach, afterEach, vi, test } from "vitest";
 import {
   render,
   screen,
@@ -16,6 +16,12 @@ import axios, { AxiosHeaders, AxiosResponse } from "axios";
 
 vi.mock("axios");
 
+const mockUserInfo: User = {
+  "role": "admin",
+  "email": "test@email.com",
+  "username": "Test Account",
+}
+
 describe("Unauthenticated LogIn Component", () => {
   const originalLocation = window.location;
 
@@ -24,9 +30,6 @@ describe("Unauthenticated LogIn Component", () => {
       data: {
         nameId: null,
         email: null,
-        id: "1234",
-        username: "Test User",
-        role: "admin",
       } as UserClaims & User,
       status: 200,
       statusText: "OK",
@@ -92,90 +95,94 @@ describe("Unauthenticated LogIn Component", () => {
   });
 });
 
-describe("Authenticated LogIn Component", () => {
-  const originalLocation = window.location;
-  const mockUserInfo: User = {
-    "role": "admin",
-    "email": "test@email.com",
-    "username": "Test Account",
-  }
+describe.each([
+  ["", mockUserInfo],
+  ["with uncached user", null],
+  ["with other cached user", {
+    email: "wronguser@gmail.co.uk",
+    role: "unauthenticated",
+    username: "John Doe"
+  }],
+])
+  ("Authenticated LogIn Component %s", (_, cachedUserInfo) => {
+    const originalLocation = window.location;
 
-  function createMockUserData(): AxiosResponse {
-    return {
-      data: {
-        nameId: "1234567890",
-        email: mockUserInfo.email,
-        id: "1234",
-        username: "Test User",
-        role: "admin",
-      } as UserClaims & User,
-      status: 200,
-      statusText: "OK",
-      headers: {},
-      config: {
-        headers: new AxiosHeaders({ "Content-Type": "text/plain" }),
-      },
-    } as AxiosResponse;
-  }
+    function createMockUserData(): AxiosResponse {
+      return {
+        data: {
+          nameId: "1234567890",
+          email: mockUserInfo.email,
+          id: "1234",
+          username: mockUserInfo.username,
+          role: mockUserInfo.role,
+        } as UserClaims & User,
+        status: 200,
+        statusText: "OK",
+        headers: {},
+        config: {
+          headers: new AxiosHeaders({ "Content-Type": "text/plain" }),
+        },
+      } as AxiosResponse;
+    }
 
-  beforeEach(async () => {
-    vi.spyOn(axios, "get").mockResolvedValue(createMockUserData());
-    window.location = { ...originalLocation, assign: vi.fn((_: string | URL) => { }) };
+    beforeEach(async () => {
+      vi.spyOn(axios, "get").mockResolvedValue(createMockUserData());
+      window.location = { ...originalLocation, assign: vi.fn((_: string | URL) => { }) };
+      localStorage.setItem("user", JSON.stringify(cachedUserInfo));
+      render(
+        <AuthProvider>
+          <MemoryRouter initialEntries={['/', '/loggedIn']}>
+            <LogIn />
+          </MemoryRouter>
+        </AuthProvider>
+      );
 
-    localStorage.setItem("user", JSON.stringify(mockUserInfo));
-    render(
-      <AuthProvider>
-        <MemoryRouter initialEntries={['/', '/loggedIn']}>
-          <LogIn />
-        </MemoryRouter>
-      </AuthProvider>
-    );
+      await act(() => axios.get); // Allows the initial useLayoutEffect to fire
+    });
 
-    await act(() => axios.get); // Allows the initial useLayoutEffect to fire
+    afterEach(() => {
+      cleanup();
+      window.location = originalLocation;
+      localStorage.clear();
+    });
+
+    it("shows different buttons when logged in", () => {
+      expect(screen.getByText(`Continue as ${mockUserInfo.username}`)).toBeTruthy();
+      expect(screen.getByText("Switch Account")).toBeTruthy();
+      expect(screen.getByText("Continue as Guest")).toBeTruthy();
+    });
+
+    it("navigates to the backend when switching accounts", () => {
+      const loginButton = screen.getByText("Switch Account");
+      expect(loginButton).toBeTruthy();
+      fireEvent.click(loginButton);
+      expect(window.location.assign).toHaveBeenCalledOnce(); // Redirect to backend auth/google/login endpoint
+    });
+
+    it("navigates to the backend when continuing as a guest", () => {
+      const guestButton = screen.getByText("Continue as Guest");
+      expect(guestButton).toBeTruthy();
+      fireEvent.click(guestButton);
+      expect(window.location.assign).toHaveBeenCalledOnce();
+    });
+
+    it("removes user from local storage when logging out", () => {
+      expect(document.getElementById("side-nav-button")).toBeTruthy();
+      expect(localStorage.getItem("user")).toBeTruthy();
+
+      // Open side nav
+      const hamburgerMenu: HTMLElement = document.getElementById("side-nav-button")!;
+      fireEvent.click(hamburgerMenu);
+
+      // Click logout button
+      const logoutButton = screen.getByText("Log Out");
+      fireEvent.click(logoutButton);
+
+      expect(window.location.assign).toHaveBeenCalledOnce(); // Redirect to backend auth/google/logout endpoint
+      expect(localStorage.getItem("user")).toBeFalsy();
+    });
   });
 
-  afterEach(() => {
-    cleanup();
-    window.location = originalLocation;
-    localStorage.clear();
-  });
-
-  it("shows different buttons when logged in", () => {
-    expect(screen.getByText(`Continue as ${mockUserInfo.username}`)).toBeTruthy();
-    expect(screen.getByText("Switch Account")).toBeTruthy();
-    expect(screen.getByText("Continue as Guest")).toBeTruthy();
-  });
-
-  it("navigates to the backend when switching accounts", () => {
-    const loginButton = screen.getByText("Switch Account");
-    expect(loginButton).toBeTruthy();
-    fireEvent.click(loginButton);
-    expect(window.location.assign).toHaveBeenCalledOnce(); // Redirect to backend auth/google/login endpoint
-  });
-
-  it("navigates to the backend when continuing as a guest", () => {
-    const guestButton = screen.getByText("Continue as Guest");
-    expect(guestButton).toBeTruthy();
-    fireEvent.click(guestButton);
-    expect(window.location.assign).toHaveBeenCalledOnce();
-  });
-
-  it("removes user from local storage when logging out", () => {
-    expect(document.getElementById("side-nav-button")).toBeTruthy();
-    expect(localStorage.getItem("user")).toBeTruthy();
-
-    // Open side nav
-    const hamburgerMenu: HTMLElement = document.getElementById("side-nav-button")!;
-    fireEvent.click(hamburgerMenu);
-
-    // Click logout button
-    const logoutButton = screen.getByText("Log Out");
-    fireEvent.click(logoutButton);
-
-    expect(window.location.assign).toHaveBeenCalledOnce(); // Redirect to backend auth/google/logout endpoint
-    expect(localStorage.getItem("user")).toBeFalsy();
-  });
-});
 
 describe("Sanity Check Tests", () => {
   it("should always pass test 1", () => {
