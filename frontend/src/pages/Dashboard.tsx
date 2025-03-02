435957c57804ba81e4fcc0c8e66f648c55164d80
LucientZ <koblenzpoisiden@gmail.com>
2025-03-02 01:34:02 -0600

    Added ui for recent family list on dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,43 +1,125 @@
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-
 import { Header, Footer } from "../components/HeaderFooter";
 
 import "../styles/Dashboard.css";
+import { ButtonGroup, Card, CardActions, CardContent, List, ListItem, Typography } from "@mui/material";
+import { Family } from "../API/API_Interfaces";
+import { memo } from "react";
+
+const dummyData: Array<Family> = [
+  {
+    name: "Test Family",
+    description: "A test family that doesn't exist and is purely for UI testing",
+    createdBy: "Test User",
+    id: "123-456-7890",
+  },
+  {
+    name: "The Amazing Digital Family",
+    description: "Another cool family that is purely for UI testing",
+    createdBy: "Bomni",
+    id: "098-765-4321",
+  },
+  {
+    name: "Test Family",
+    description: "A test family that doesn't exist",
+    createdBy: "Test User",
+    id: "927-564-2221",
+  },
+  {
+    name: "The funniest family",
+    description: "They call me the UI tester",
+    createdBy: "Test User",
+    id: "123-456-7890",
+  },
+  {
+    name: "This is a reaally loooongggg family name just to make sure it doesn't overflow",
+    description: "This is a really long description to make sure that the component doesn't overflow when there's too much text on screen. This is on purpose and not an accident. Hopefully this is long enough now and I don't have to keep going",
+    createdBy: "Test User",
+    id: "123-456-7890",
+  },
+  {
+    name: "XSS test <script>alert('Uh Oh')</script>",
+    description: "<script>alert('Uh Oh')</script>",
+    createdBy: "Test User",
+    id: "123-456-7890",
+  },
+  {
+    name: "Family at the bottom",
+    description: "This is used to test the situation when a family has \"overflowed\" the element. This happens on certain screen resolutions.",
+    createdBy: "Test User",
+    id: "123-456-7890",
+  },
+]
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const handleClickFam = () => navigate("/familypage");
+  const handleClickFamily = () => navigate("/familypage");
   const handleClickSettings = () => navigate("/settings");
 
-  const style = {
-    height: "75px",
-    width: "500px",
+  const buttonStyle = {
+    height: "5rem",
+    width: "90%",
   };
-  return (
-    <section className="layoutLoggedIn">
-      <div className="L1LoggedIn">
-        <Header></Header>
-      </div>
 
-      <div className="M4">
-        <ButtonGroup orientation="vertical" variant="contained">
-          <Button sx={style} onClick={handleClickFam}>
-            Families
+  return (
+    <div className="layout-dashboard">
+      <header>
+        <Header />
+      </header>
+      <section className="content-dashboard">
+        <div className="dashboard-navigation-buttons">
+          <Button variant="contained" sx={buttonStyle} onClick={handleClickFamily}>
+            Family Editor
           </Button>
-          <p></p>
-          <Button sx={style} onClick={handleClickSettings}>
+          <Button variant="contained" sx={buttonStyle} onClick={handleClickSettings}>
             Settings
           </Button>
-        </ButtonGroup>
-      </div>
-
-      <div className="L9LoggedIn">
-        <Footer></Footer>
-      </div>
-    </section>
+        </div>
+        <div className="dashboard-family-explorer">
+          <Typography variant="h5">
+            Recent Mechanism Families
+          </Typography>
+          <List>
+            {dummyData.map((family: Family, index: number) => {
+              return (
+                <FamilyInfoCard key={`${family.id}-${index}`} family={family} />
+              );
+            })}
+          </List>
+        </div>
+      </section>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 };
+
+const FamilyInfoCard = memo(function FamilyInfoCard({ family }: { family: Family }) {
+  return (
+    <ListItem>
+      <Card sx={{ flex: 1 }} variant="outlined">
+        <CardContent>
+          <Typography sx={{ fontWeight: "bold" }} noWrap variant="h6" color="textPrimary">
+            {family.name}
+          </Typography>
+          <Typography noWrap variant="inherit" color="textSecondary">
+            {family.createdBy}
+          </Typography>
+          <Typography sx={{ marginY: 1 }}>
+            {family.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ButtonGroup variant="outlined">
+            <Button color="info" onClick={() => alert("Not Implemented")} size="small">Learn More</Button>
+            <Button color="primary" onClick={() => alert("Not Implemented")} size="small">Edit Family</Button>
+          </ButtonGroup>
+        </CardActions>
+      </Card>
+    </ListItem>
+  )
+});
 
 export default Dashboard;
