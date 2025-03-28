f369bf34721300d4d638bae6b07aaece2db652fe
LucientZ <koblenzpoisiden@gmail.com>
2025-03-28 01:21:26 -0500

    Attempt at ensuring environment variables are the issue

--- backend/mstests/Controllers/DBConnection.cs
+++ backend/mstests/Controllers/DBConnection.cs
@@ -6,10 +6,10 @@ namespace ChemistryCafeAPI.Tests
 {
     public class DBConnection
     {
-        private static string server = Environment.GetEnvironmentVariable("MYSQL_SERVER") ?? "localhost";
-        private static string user = System.Environment.GetEnvironmentVariable("MYSQL_USER") ?? "chemistrycafedev";
-        private static string password = System.Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "chemistrycafe";
-        private static string database = System.Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "chemistry_db";
+        private static string server = "localhost";
+        private static string user = "chemistrycafedev";
+        private static string password = "chemistrycafe";
+        private static string database = "chemistry_db";
         private static string port = "3306"; // Default MySQL port
         private static string connectionString = $"Server={server};Port={port};Database={database};User={user};Password={password};AllowUserVariables=True;UseAffectedRows=False;";
         private static DbContextOptions<ChemistryDbContext> options = new DbContextOptionsBuilder<ChemistryDbContext>()
