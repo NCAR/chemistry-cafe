54610fb3e92928c0e4db3825aad16fa5e1d70e67
KailiF <kfogle6782@tamu.edu>
2025-04-02 17:21:29 -0500

    Removed unneeded content from API and API tests.

--- frontend/src/API/API_UpdateMethods.tsx
+++ frontend/src/API/API_UpdateMethods.tsx
@@ -2,97 +2,11 @@
 
 import axios from "axios";
 import {
-  APIFamily,
-  APIMechanism,
-  APISpecies,
-  APIReaction,
-  APIUser,
-  APIProperty,
+  APIUser
 } from "./API_Interfaces";
 import { BASE_URL } from "./API_config";
 
-// Update a family
-export async function updateFamily(family: APIFamily) {
-  try {
-    const response = await axios.put(
-      `${BASE_URL}/families/${family.id}`,
-      family,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return response.data as APIFamily;
-  } catch (error: any) {
-    console.error(`Error updating family ${family}: ${error.message}`, error);
-    throw new Error("Failed to update family. Please try again later.");
-  }
-}
-
-// Update a mechanism
-export async function updateMechanism(mechanism: APIMechanism) {
-  try {
-    const response = await axios.put(
-      `${BASE_URL}/mechanism/${mechanism.id}`,
-      mechanism,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return response.data as APIMechanism;
-  } catch (error: any) {
-    console.error(
-      `Error updating mechanism ${mechanism}: ${error.message}`,
-      error,
-    );
-    throw new Error("Failed to update mechanism. Please try again later.");
-  }
-}
-
-// Update a species
-export async function updateSpecies(species: APISpecies) {
-  try {
-    const response = await axios.put(
-      `${BASE_URL}/species/${species.id}`,
-      species,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return response.data as APISpecies;
-  } catch (error: any) {
-    console.error(`Error updating species ${species}: ${error.message}`, error);
-    throw new Error("Failed to update species. Please try again later.");
-  }
-}
-
-// Update a reaction
-export async function updateReaction(reaction: APIReaction) {
-  try {
-    const response = await axios.put(
-      `${BASE_URL}/reactions/${reaction.id}`,
-      reaction,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return response.data as APIReaction;
-  } catch (error: any) {
-    console.error(
-      `Error updating reaction ${reaction}: ${error.message}`,
-      error,
-    );
-    throw new Error("Failed to update reaction. Please try again later.");
-  }
-}
-
+// Update a user
 export async function updateUser(id: string, user: APIUser) {
   try {
     const response = await axios.put(`${BASE_URL}/users/${id}`, user, {
@@ -107,23 +21,3 @@ export async function updateUser(id: string, user: APIUser) {
   }
 }
 
-export async function updateProperty(property: APIProperty) {
-  try {
-    const response = await axios.put(
-      `${BASE_URL}/properties/${property.id}`,
-      property,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-    return response.data as APIProperty;
-  } catch (error: any) {
-    console.error(
-      `Error updating property ${property}: ${error.message}`,
-      error,
-    );
-    throw new Error("Failed to update property. Please try again later.");
-  }
-}
--- frontend/test/API_DeleteMethods.test.tsx
+++ frontend/test/API_DeleteMethods.test.tsx
@@ -2,12 +2,7 @@ import { describe, expect, it, vi } from "vitest";
 import type { Mock } from "vitest";
 import axios, { AxiosResponse } from "axios";
 import {
-  deleteFamily,
-  deleteMechanism,
-  deleteSpecies,
-  deleteReaction,
   deleteUser,
-  deleteProperty,
 } from "../src/API/API_DeleteMethods";
 
 // Mock axios using vitest's built-in mock function
@@ -26,141 +21,6 @@ describe("API delete functions tests", () => {
       config: {},
     } as AxiosResponse;
   }
-
-  it("should successfully delete a family", async () => {
-    const mockedDelete = vi
-      .spyOn(axios, "delete")
-      .mockResolvedValue(createMockResponse()) as Mock;
-
-    const id = "12345";
-    const result = await deleteFamily(id);
-
-    expect(mockedDelete).toHaveBeenCalledWith(
-      `http://localhost:8080/api/families/${id}`,
-      {
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-    expect(result).toEqual(mockResponseData);
-  });
-
-  it("should handle error correctly for deleteFamily", async () => {
-    const id = "12345";
-    const errorMessage = "Request failed with status code 404";
-
-    (
-      axios.delete as typeof axios.delete & { mockRejectedValueOnce: Function }
-    ).mockRejectedValueOnce(new Error(errorMessage));
-    // Assert the function throws the correct error
-    await expect(deleteFamily(id)).rejects.toThrow(
-      "Failed to delete family. Please try again later.",
-    );
-
-    expect(axios.delete).toHaveBeenCalledWith(`${BASE_URL}/families/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  });
-
-  it("should successfully delete a mechanism", async () => {
-    const mockedDelete = vi
-      .spyOn(axios, "delete")
-      .mockResolvedValue(createMockResponse()) as Mock;
-
-    const id = "12345";
-    const result = await deleteMechanism(id);
-
-    expect(mockedDelete).toHaveBeenCalledWith(
-      `http://localhost:8080/api/mechanism/${id}`,
-    );
-    expect(result).toEqual(mockResponseData);
-  });
-
-  it("should handle error correctly for deleteMechanism", async () => {
-    const id = "12345";
-    const errorMessage = "Request failed with status code 404";
-
-    (
-      axios.delete as typeof axios.delete & { mockRejectedValueOnce: Function }
-    ).mockRejectedValueOnce(new Error(errorMessage));
-    // Assert the function throws the correct error
-    await expect(deleteMechanism(id)).rejects.toThrow(
-      "Failed to delete mechanism. Please try again later.",
-    );
-  });
-
-  it("should successfully delete a species", async () => {
-    const mockedDelete = vi
-      .spyOn(axios, "delete")
-      .mockResolvedValue(createMockResponse()) as Mock;
-
-    const id = "12345";
-    const result = await deleteSpecies(id);
-
-    expect(mockedDelete).toHaveBeenCalledWith(
-      `http://localhost:8080/api/species/${id}`,
-      {
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-    expect(result).toEqual(mockResponseData);
-  });
-
-  it("should handle error correctly for deleteSpecies", async () => {
-    const id = "12345";
-    const errorMessage = "Request failed with status code 404";
-
-    (
-      axios.delete as typeof axios.delete & { mockRejectedValueOnce: Function }
-    ).mockRejectedValueOnce(new Error(errorMessage));
-    // Assert the function throws the correct error
-    await expect(deleteSpecies(id)).rejects.toThrow(
-      "Failed to delete species. Please try again later.",
-    );
-
-    expect(axios.delete).toHaveBeenCalledWith(`${BASE_URL}/species/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  });
-
-  it("should successfully delete a reaction", async () => {
-    const mockedDelete = vi
-      .spyOn(axios, "delete")
-      .mockResolvedValue(createMockResponse()) as Mock;
-
-    const id = "12345";
-    const result = await deleteReaction(id);
-
-    expect(mockedDelete).toHaveBeenCalledWith(
-      `http://localhost:8080/api/reactions/${id}`,
-      {
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-    expect(result).toEqual(mockResponseData);
-  });
-
-  it("should handle error correctly for deleteReaction", async () => {
-    const id = "12345";
-    const errorMessage = "Request failed with status code 404";
-
-    (
-      axios.delete as typeof axios.delete & { mockRejectedValueOnce: Function }
-    ).mockRejectedValueOnce(new Error(errorMessage));
-    // Assert the function throws the correct error
-    await expect(deleteReaction(id)).rejects.toThrow(
-      "Failed to delete reaction. Please try again later.",
-    );
-
-    expect(axios.delete).toHaveBeenCalledWith(`${BASE_URL}/reactions/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  });
 
   it("should successfully delete a user", async () => {
     const mockedDelete = vi
@@ -198,39 +58,4 @@ describe("API delete functions tests", () => {
     });
   });
 
-  it("should successfully delete a property", async () => {
-    const mockedDelete = vi
-      .spyOn(axios, "delete")
-      .mockResolvedValue(createMockResponse()) as Mock;
-
-    const id = "12345"; // Test property ID
-    const result = await deleteProperty(id);
-
-    expect(mockedDelete).toHaveBeenCalledWith(
-      `http://localhost:8080/api/properties/${id}`, // URL based on the deleteProperty function
-      {
-        headers: { "Content-Type": "application/json" }, // Request headers
-      },
-    );
-    expect(result).toEqual(mockResponseData); // Ensure the result matches the expected response
-  });
-
-  it("should handle error correctly for deleteProperty", async () => {
-    const id = "12345";
-    const errorMessage = "Request failed with status code 404";
-
-    (
-      axios.delete as typeof axios.delete & { mockRejectedValueOnce: Function }
-    ).mockRejectedValueOnce(new Error(errorMessage));
-    // Assert the function throws the correct error
-    await expect(deleteProperty(id)).rejects.toThrow(
-      "Failed to delete property. Please try again later.",
-    );
-
-    expect(axios.delete).toHaveBeenCalledWith(`${BASE_URL}/properties/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  });
 });
