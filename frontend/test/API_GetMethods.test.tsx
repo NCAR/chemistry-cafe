3485680ad01b84162668792ff18bfe63ccb2e40f
LucientZ <koblenzpoisiden@gmail.com>
2025-04-05 01:41:17 -0500

    Updated get methods

--- frontend/test/API_GetMethods.test.tsx
+++ frontend/test/API_GetMethods.test.tsx
@@ -2,177 +2,161 @@ import { describe, expect, it, vi } from "vitest";
 import type { Mock } from "vitest";
 import axios, { AxiosHeaders, AxiosResponse } from "axios";
 import {
-  downloadOAJSON,
-  downloadOAYAML,
-  downloadOAMusicbox,
-  getUsers,
+  getAllUsers,
+  getCurrentUser,
   getUserByEmail,
   getUserById,
 } from "../src/API/API_GetMethods";
-import { APIMechanism } from "../src/API/API_Interfaces";
+import { APIMechanism, APIUser } from "../src/API/API_Interfaces";
+import { AUTH_URL, BASE_URL } from "../src/API/API_config";
 
 // Mock axios using vitest's built-in mock function
 vi.mock("axios");
 
-describe("API get functions tests", () => {
-  const mockResponseData = { success: true };
+const allUsers: Array<APIUser> = [
+  {
+    id: "1-1-1-1-1",
+    email: "test1@email.com",
+    username: "Test Admin",
+    role: "admin",
+  },
+  {
+    id: "2-2-2-2-2",
+    email: "test2@example.com",
+    username: "Test User",
+    role: "unverified",
+  },
+  {
+    id: "3-3-3-3-3",
+    email: "test3@test.edu",
+    username: "verified test user",
+    role: "verified",
+  },
+]
 
-  function createMockResponse() {
+describe("User get functions", () => {
+  function createSuccessMockResponse(data: any): AxiosResponse {
     return {
-      data: mockResponseData,
+      data: data,
       status: 200,
       statusText: "OK",
       headers: {},
       config: {
-        headers: new AxiosHeaders({ "Content-Type": "text/plain" }),
+        headers: new AxiosHeaders({ "Content-Type": "application/json" }),
       },
     } as AxiosResponse;
   }
+  describe("getAllUsers", () => {
 
-  // Tests for downloadOAJSON
-  it("should successfully get OAJSON with valid tag_mechanism_uuid", async () => {
-    const mockedGet = vi
-      .spyOn(axios, "get")
-      .mockResolvedValueOnce(createMockResponse()) as Mock;
+    it("Gets all users when called", async () => {
+      const mockedGet = vi
+        .spyOn(axios, "get")
+        .mockResolvedValue(createSuccessMockResponse(allUsers)) as Mock;
+      const result = await getAllUsers();
 
-    const tag_mechanism_uuid = "valid-uuid";
-    const result = await downloadOAJSON(tag_mechanism_uuid);
+      expect(mockedGet).toHaveBeenCalledWith(`${BASE_URL}/users`);
+      expect(result).toEqual(allUsers);
+    });
 
-    expect(mockedGet).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/json`,
-      {
-        headers: { "Content-Type": "text/plain" },
-        responseType: "text",
-      },
-    );
-
-    expect(result).toBe(mockResponseData);
+    it("Handles errors by throwing returning empty list", async () => {
+      const errorMessage = "Request failed with status code 404";
+      (
+        axios.get as typeof axios.get & { mockRejectedValueOnce: Function }
+      ).mockRejectedValueOnce(new Error(errorMessage));
+      expect(await getAllUsers()).toEqual([]);
+    });
   });
 
-  it("should return an empty string when tag_mechanism_uuid is not provided", async () => {
-    const mockedGet = vi.spyOn(axios, "get");
+  describe("getUserByEmail", () => {
+    it("Gets a user by email", async () => {
+      for (const user of allUsers) {
+        if (!user.email) {
+          continue;
+        }
 
-    const result = await downloadOAJSON();
+        const mockedGet = vi
+          .spyOn(axios, "get")
+          .mockResolvedValue(createSuccessMockResponse(user)) as Mock;
+        const result = await getUserByEmail(user.email);
 
-    expect(result).toBe("");
-    expect(mockedGet).not.toHaveBeenCalled();
+        expect(mockedGet).toHaveBeenCalledWith(`${BASE_URL}/users/email/${user.email}`);
+        expect(result).toEqual(user);
+      }
+    });
 
-    mockedGet.mockRestore();
+    it("Handles errors by returning null", async () => {
+      for (const user of allUsers) {
+        if (!user.email) {
+          continue;
+        }
+
+        const errorMessage = "Request failed with status code 404";
+        (
+          axios.get as typeof axios.get & { mockRejectedValueOnce: Function }
+        ).mockRejectedValueOnce(new Error(errorMessage));
+        expect(await getUserByEmail(user.email)).toEqual(null);
+      }
+    });
   });
 
-  it("should handle error correctly for downloadOAJSON", async () => {
-    const mockError = new Error("Network error");
-    vi.spyOn(axios, "get").mockRejectedValueOnce(mockError);
+  describe("getUserById", () => {
+    it("Gets a user by id", async () => {
+      for (const user of allUsers) {
+        if (!user.id) {
+          continue;
+        }
 
-    const tag_mechanism_uuid = "invalid-uuid";
-    await expect(downloadOAJSON(tag_mechanism_uuid)).rejects.toThrow(
-      "Network error",
-    );
+        const mockedGet = vi
+          .spyOn(axios, "get")
+          .mockResolvedValue(createSuccessMockResponse(user)) as Mock;
+        const result = await getUserById(user.id);
 
-    expect(axios.get).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/json`,
-      {
-        headers: { "Content-Type": "text/plain" },
-        responseType: "text",
-      },
-    );
+        expect(mockedGet).toHaveBeenCalledWith(`${BASE_URL}/users/id/${user.id}`);
+        expect(result).toEqual(user);
+      }
+    });
+
+    it("Handles errors by returning null", async () => {
+      for (const user of allUsers) {
+        if (!user.id) {
+          continue;
+        }
+
+        const errorMessage = "Request failed with status code 404";
+        (
+          axios.get as typeof axios.get & { mockRejectedValueOnce: Function }
+        ).mockRejectedValueOnce(new Error(errorMessage));
+        expect(await getUserById(user.id)).toEqual(null);
+      }
+    });
   });
 
-  // Tests for downloadOAYAML
-  it("should successfully get OAYAML with valid tag_mechanism_uuid", async () => {
-    const mockedGet = vi
-      .spyOn(axios, "get")
-      .mockResolvedValueOnce(createMockResponse()) as Mock;
+  describe("getCurrentUser", () => {
+    it("Gets the current user", async () => {
+      for (const user of allUsers) {
+        const mockedGet = vi
+          .spyOn(axios, "get")
+          .mockResolvedValue(createSuccessMockResponse(user)) as Mock;
+        const result = await getCurrentUser();
 
-    const tag_mechanism_uuid = "valid-uuid";
-    const result = await downloadOAYAML(tag_mechanism_uuid);
+        expect(mockedGet).toHaveBeenCalledWith(
+          `${AUTH_URL}/google/whoami`,
+          {
+            withCredentials: true,
+          },
+        );
+        expect(result).toEqual(user);
+      }
+    });
 
-    expect(mockedGet).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/yaml`,
-      {
-        headers: { "Content-Type": "text/plain" },
-        responseType: "text",
-      },
-    );
-
-    expect(result).toBe(mockResponseData);
-  });
-
-  it("should return an empty string when tag_mechanism_uuid is not provided for OAYAML", async () => {
-    const mockedGet = vi.spyOn(axios, "get");
-
-    const result = await downloadOAYAML();
-
-    expect(result).toBe("");
-    expect(mockedGet).not.toHaveBeenCalled();
-
-    mockedGet.mockRestore();
-  });
-
-  it("should handle error correctly for downloadOAYAML", async () => {
-    const mockError = new Error("Network error");
-    vi.spyOn(axios, "get").mockRejectedValueOnce(mockError);
-
-    const tag_mechanism_uuid = "invalid-uuid";
-    await expect(downloadOAYAML(tag_mechanism_uuid)).rejects.toThrow(
-      "Network error",
-    );
-
-    expect(axios.get).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/yaml`,
-      {
-        headers: { "Content-Type": "text/plain" },
-        responseType: "text",
-      },
-    );
-  });
-
-  // Tests for downloadOAMusicbox
-  it("should successfully get OAMusicbox with valid tag_mechanism_uuid", async () => {
-    const mockedGet = vi
-      .spyOn(axios, "get")
-      .mockResolvedValueOnce(createMockResponse()) as Mock;
-
-    const tag_mechanism_uuid = "valid-uuid";
-    const result = await downloadOAMusicbox(tag_mechanism_uuid);
-
-    expect(mockedGet).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/musicbox`,
-      {
-        responseType: "arraybuffer",
-        headers: { Accept: "application/zip" },
-      },
-    );
-
-    expect(result).toBe(mockResponseData);
-  });
-
-  it("should return an empty string when tag_mechanism_uuid is not provided for OAMusicbox", async () => {
-    const mockedGet = vi.spyOn(axios, "get");
-
-    const result = await downloadOAMusicbox();
-
-    expect(result).toBe("");
-    expect(mockedGet).not.toHaveBeenCalled();
-
-    mockedGet.mockRestore();
-  });
-
-  it("should handle error correctly for downloadOAMusicbox", async () => {
-    const mockError = new Error("Network error");
-    vi.spyOn(axios, "get").mockRejectedValueOnce(mockError);
-
-    const tag_mechanism_uuid = "invalid-uuid";
-    await expect(downloadOAMusicbox(tag_mechanism_uuid)).rejects.toThrow(
-      "Network error",
-    );
-
-    expect(axios.get).toHaveBeenCalledWith(
-      `http://localhost:8080/api/openatmos/mechanism/${tag_mechanism_uuid}/musicbox`,
-      {
-        responseType: "arraybuffer",
-        headers: { Accept: "application/zip" },
-      },
-    );
+    it("Handles errors by returning null", async () => {
+      for (const _ of allUsers) {
+        const errorMessage = "Request failed with status code 404";
+        (
+          axios.get as typeof axios.get & { mockRejectedValueOnce: Function }
+        ).mockRejectedValueOnce(new Error(errorMessage));
+        expect(await getCurrentUser()).toEqual(null);
+      }
+    });
   });
 });
