4eae7d8c3f0d03f560ff20ff63c69681e4696f2a
quasar32 <fontejam@tamu.edu>
2025-04-24 20:17:22 -0500

    added invalid family mechanism test

--- backend/mstests/Controllers/MecahnismControllerTests.cs
+++ backend/mstests/Controllers/MecahnismControllerTests.cs
@@ -15,11 +15,11 @@ namespace ChemistryCafeAPI.Tests
         private static ChemistryDbContext _context = DBConnection.Context;
         private static User? _user;
         private static Family? _family;
-        private static Mechanism? _phase;
+        private static Mechanism? _mechanism;
         private static string? _nameIdentifier;
 
-        private MechanismService _phaseService; 
-        private MechanismController _phaseController; 
+        private MechanismService _mechanismService; 
+        private MechanismController _mechanismController; 
         private UserService _userService; 
         private FamilyService _familyService; 
 
@@ -39,15 +39,15 @@ namespace ChemistryCafeAPI.Tests
         {
             _context = DBConnection.Context;
             _userService = new UserService(_context);
-            _phaseService = new MechanismService(_context);
-            _phaseController = new MockedMechanismController(_phaseService);
+            _mechanismService = new MechanismService(_context);
+            _mechanismController = new MockedMechanismController(_mechanismService);
             _familyService = new FamilyService(_context, _userService);
         }
 
         private async Task AsyncInit()
         {
-            var googleId = "phase-sample-google-id";
-            var email = "phase-test@fake-website.com";
+            var googleId = "mechanism-sample-google-id";
+            var email = "mechanism-test@fake-website.com";
             _user = await _userService.SignIn(googleId, email);
             _family = new Family
             {
@@ -70,7 +70,7 @@ namespace ChemistryCafeAPI.Tests
         [TestMethod]
         public async Task GetMechanisms()
         {
-            var actionResult = await _phaseController.GetMechanisms(null);
+            var actionResult = await _mechanismController.GetMechanisms(null);
             Assert.IsNotNull(actionResult);
             var okResult = actionResult.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
@@ -79,67 +79,75 @@ namespace ChemistryCafeAPI.Tests
         [TestMethod]
         public async Task CreateMechanism()
         {
-            _phase = new Mechanism 
+            _mechanism = new Mechanism 
             {
                 Name = "TestMechanism",
                 Description = "From MechanismControllerTests.cs",
                 CreatedDate = DateTime.UtcNow 
             };
-            var actionResult = await _phaseController.CreateMechanism(_phase, _family.Id);
+            var actionResult = await _mechanismController.CreateMechanism(_mechanism, _family.Id);
             Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult));
             var createdAtActionResult = actionResult.Result as CreatedAtActionResult;
             Assert.IsNotNull(createdAtActionResult);
             var returnedMechanism = createdAtActionResult.Value as Mechanism;
             Assert.IsNotNull(returnedMechanism);
-            Assert.AreEqual(_phase.Name, returnedMechanism.Name);
-            Assert.AreEqual(_phase.Description, returnedMechanism.Description);
+            Assert.AreEqual(_mechanism.Name, returnedMechanism.Name);
+            Assert.AreEqual(_mechanism.Description, returnedMechanism.Description);
             Assert.AreEqual(_family.Id, returnedMechanism.FamilyId); 
-            _phase = returnedMechanism;
+            _mechanism = returnedMechanism;
         }
 
         [TestMethod]
         public async Task GetMechanism()
         {
-            var actionResult = await _phaseController.GetMechanism(_phase.Id);
+            var actionResult = await _mechanismController.GetMechanism(_mechanism.Id);
             Assert.IsNotNull(actionResult);
             var okResult = actionResult.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
             var returnedMechanism = okResult.Value as Mechanism;
             Assert.IsNotNull(returnedMechanism);
-            Assert.AreEqual(_phase.Id, returnedMechanism.Id);
-            Assert.AreEqual(_phase.Name, returnedMechanism.Name);
-            Assert.AreEqual(_phase.Description, returnedMechanism.Description);
-            Assert.AreEqual(_phase.FamilyId, returnedMechanism.FamilyId);
+            Assert.AreEqual(_mechanism.Id, returnedMechanism.Id);
+            Assert.AreEqual(_mechanism.Name, returnedMechanism.Name);
+            Assert.AreEqual(_mechanism.Description, returnedMechanism.Description);
+            Assert.AreEqual(_mechanism.FamilyId, returnedMechanism.FamilyId);
         }
 
         [TestMethod]
         public async Task GetMechanismsFromFamily()
         {
-            var actionResult = await _phaseController.GetMechanisms(_family.Id);
+            var actionResult = await _mechanismController.GetMechanisms(_family.Id);
             Assert.IsNotNull(actionResult);
             var okResult = actionResult.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
-            var phaseList = okResult.Value as IEnumerable<Mechanism>;
-            Assert.IsNotNull(phaseList);
-            Assert.IsTrue(phaseList.Count() >= 1);
+            var mechanismList = okResult.Value as IEnumerable<Mechanism>;
+            Assert.IsNotNull(mechanismList);
+            Assert.IsTrue(mechanismList.Count() >= 1);
+        }
+
+        [TestMethod]
+        public async Task GetMechanismsFromInvalidFamily()
+        {
+            var actionResult = await _mechanismController.GetMechanisms(Guid.NewGuid());
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundObjectResult));
         }
 
         [TestMethod]
         public async Task UpdateMechanism()
         {
-            _phase.Name = "UPDATEDTest";
-            _phase.Description = "UPDATEDDesc";
-            var actionResult = await _phaseController.UpdateMechanism(_phase.Id, _phase);
+            _mechanism.Name = "UPDATEDTest";
+            _mechanism.Description = "UPDATEDDesc";
+            var actionResult = await _mechanismController.UpdateMechanism(_mechanism.Id, _mechanism);
             Assert.IsNotNull(actionResult);
             Assert.IsInstanceOfType(actionResult.Result, typeof(OkObjectResult));
             var createdAtActionResult = actionResult.Result as OkObjectResult;
             Assert.IsNotNull(createdAtActionResult);
             var returnedMechanism = createdAtActionResult.Value as Mechanism;
             Assert.IsNotNull(returnedMechanism);
-            Assert.AreEqual(_phase.Id, returnedMechanism.Id);
-            Assert.AreEqual(_phase.Name, returnedMechanism.Name);
-            Assert.AreEqual(_phase.Description, returnedMechanism.Description);
-            Assert.AreEqual(_phase.FamilyId, returnedMechanism.FamilyId); 
+            Assert.AreEqual(_mechanism.Id, returnedMechanism.Id);
+            Assert.AreEqual(_mechanism.Name, returnedMechanism.Name);
+            Assert.AreEqual(_mechanism.Description, returnedMechanism.Description);
+            Assert.AreEqual(_mechanism.FamilyId, returnedMechanism.FamilyId); 
         }
 
         [TestMethod]
@@ -147,17 +155,17 @@ namespace ChemistryCafeAPI.Tests
         {
             await _familyService.UpdateFamilyAsync(_family.Id, _family, _nameIdentifier);
             _family.Mechanisms.Clear();
-            await _phaseController.DeleteMechanism(_phase.Id);
-            var actionResult = await _phaseController.GetMechanism(_phase.Id);
+            await _mechanismController.DeleteMechanism(_mechanism.Id);
+            var actionResult = await _mechanismController.GetMechanism(_mechanism.Id);
             Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundObjectResult));
-            _phase = null;
+            _mechanism = null;
         }
 
         private async Task AsyncCleanup()
         {
-            if (_phase != null)
+            if (_mechanism != null)
             {
-                await _phaseService.DeleteMechanismAsync(_phase.Id, _nameIdentifier);
+                await _mechanismService.DeleteMechanismAsync(_mechanism.Id, _nameIdentifier);
             }
             if (_family != null)
             {
